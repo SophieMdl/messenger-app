99c293fd2902e4b9abe1e9b7ebe5d920a41d3027
sophie.michaudel@gmail.com <sophie.michaudel@gmail.com>
2020-03-11 14:15:26 +0100

    display and send message

--- src/components/Messenger.js
+++ src/components/Messenger.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { initialMessages } from "../messages.js";
+import { Card, Container, Button, TextField } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+
+import "./Messenger.css";
+
+const myName = "Loki";
 
 const Messenger = () => {
-  return <div>Messagerie</div>;
+  const [messages, setMessages] = useState(initialMessages);
+  const [newMessage, setNewMessage] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const newMessageWithData = {
+      id: messages.length + 2,
+      content: newMessage,
+      author: myName,
+      date: new Date(),
+      private: true
+    };
+    setMessages([...messages, newMessageWithData]);
+    setNewMessage("");
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <h2>Messages</h2>
+      {messages.map(msg => (
+        <Card className="message" key={msg.id} variant="outlined">
+          {msg.content}
+        </Card>
+      ))}
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          className="new-message"
+          multiline
+          id="new-message"
+          label="Mon message"
+          value={newMessage}
+          onChange={e => setNewMessage(e.target.value)}
+          rows={4}
+          margin="dense"
+          required
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          startIcon={<SendIcon />}
+        >
+          Envoyer
+        </Button>
+      </form>
+    </Container>
+  );
 };
 
 export default Messenger;
---
+++ src/messages.js
@@ -0,0 +1,23 @@
+export const initialMessages = [
+  {
+    id: 1,
+    content: "Bonjour",
+    author: "Rémy Dupond",
+    date: "18 / 01 / 2020",
+    private: false
+  },
+  {
+    id: 2,
+    content: "Je suis ton père",
+    author: "Dark Vador",
+    date: "20 / 01 / 2020",
+    private: true
+  },
+  {
+    id: 3,
+    content: "Winter is coming",
+    author: "John Snow",
+    date: "20 / 12 / 2020",
+    private: false
+  }
+];
